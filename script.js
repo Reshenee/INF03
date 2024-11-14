fe8db31f664059b56a4e93b844155ef2e41502c7
Roman Denisov <115945551+Reshenee@users.noreply.github.com>
2024-11-14 19:26:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let przeslijBtn = document.getElementById("przeslij");
+
+przeslijBtn.addEventListener("click", () => {
+    let imie = document.getElementById("imie").value;
+    let nazwisko = document.getElementById("nazwisko").value;
+    let email = document.getElementById("email").value;
+    let zgloszenie = document.getElementById("zgloszenie").value;
+
+    let tresc = document.getElementById("tresc");
+    let zaznaczenieZapoznania = document.getElementById("zaznaczenie").checked;
+    if (!zaznaczenieZapoznania){
+        tresc.innerText = "Musisz zapoznac sie regulaminem"
+        tresc.setAttribute("style", "color:red");
+    } else {
+        tresc.innerHTML = imie.toUpperCase() + " " + nazwisko.toUpperCase() + "<br> Tresc twojej sprawy: " + zgloszenie;
+        tresc.setAttribute("style", "color:navy");
+        
+    }
+});
